c9fd4dbc8ad20097a6a09eb09576c3d1f4d2dda3
Dulana Dewmin <74188883+DulanaDewmin@users.noreply.github.com>
2023-10-09 19:19:38 +0530

    display tasks in home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list/screens/add.dart';
 
@@ -9,6 +11,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String uid = '';
+  @override
+  void initState() {
+    getuid();
+    super.initState();
+  }
+
+getuid() async{
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final User user = await auth.currentUser!;
+  setState(() {
+    uid = user.uid;
+  });
+
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +34,34 @@ class _HomeState extends State<Home> {
         title: Text("To-Do List")
       ),
       body: Container(
+        padding: EdgeInsets.all(10),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
+        child: StreamBuilder(stream: FirebaseFirestore.instance.collection('tasks').doc(uid).collection('mytasks').snapshots(), builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          else{
+             final docs = snapshot.data!;
+             return ListView.builder(itemCount: docs.size,
+             itemBuilder: (context, Index){
+              return Container(
+                margin: EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Color.fromARGB(255, 63, 138, 100), ),
+                height: 90,
+                
+                child: Column(
+                  children: [
+                    Text(docs.docs[Index]['name']),
+                    //Text(docs.docs[Index]['description']),
+                  ],
+                ),
+              );
+             },);
+          }
+        }),
         color: Color.fromARGB(255, 116, 112, 111),
       ),
       floatingActionButton: FloatingActionButton(
